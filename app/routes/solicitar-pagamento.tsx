187445b232f6dda5c78166b0b7ab71d67c4aaeff
Italo Sergio <italosergio@mail.com>
2024-11-13 14:32:55 -0300

    refactore: data request from remix loader until instead of requesnt on useEffect function

--- app/routes/solicitar-pagamento.tsx
+++ app/routes/solicitar-pagamento.tsx
@@ -1,36 +1,39 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "@remix-run/react";
+import { json, useLoaderData, useNavigate } from "@remix-run/react";
 import ValorInput from "~/components/ValorInput";
 import FornecedorAutocomplete from "~/components/FornecedorAutocomplete";
 import Unauthorized from "~/components/Unauthorized";
 import { getProjects } from "../api/firebaseConnection";
 
+export async function loader() {
+  const data = await getProjects();
+  return json(data);
+}
+
 export default function SolicitarPagamento() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data)
   const navigate = useNavigate();
   const [descricao, setDescricao] = useState("");
   const [valor, setValor] = useState("");
   const [fornecedor, setFornecedor] = useState("");
-  const [projetos, setProjetos] = useState([]);
+  const [projectsNames, setProjectsNames] = useState(
+    Object.values(data).map((item: any) => item?.name)
+  );
   const [fornecedores, setFornecedores] = useState<any[]>([]);
-
-  useEffect(() => {
-    async function fetchProjects() {
-      try {
-        const data =  await getProjects();
-        setProjetos(data);
-      } catch (error) {
-        console.error("Erro ao buscar projetos:", error);
-      }
-    }
-    fetchProjects();
-  }, []);
 
   return (
     <div className="container">
       <h2 className="text-primary"> Solicitar Pagamento</h2>
       <div className="form-group">
         <label className="form-label">Projeto:</label>
-        {/* Renderizar projetos aqui based on fetched data in 'projetos' */}
+        <select name="projects">
+          {projectsNames.map((name, index) => (
+            <option key={index} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="form-group">
         <label className="form-label">Fornecedor:</label>
@@ -53,7 +56,7 @@ export default function SolicitarPagamento() {
         <label className="form-label">Valor:</label>
         <ValorInput valor={valor} setValor={setValor} />
       </div>
-      <button className="button-full" onClick={() => console.log(projetos)}>
+      <button className="button-full" onClick={() => console.log(projectsNames)}>
         Enviar Solicitação
       </button>
       <button className="button-secondary-full" onClick={() => navigate(-1)}>
