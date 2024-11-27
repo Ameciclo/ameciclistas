65c0115c111b65f2ef2666eda50adf1680b26350
Italo Sergio <italosergio@mail.com>
2024-11-27 01:47:11 -0300

    tenta corrigir erro de autenticação de usuários no ambiente de produção - visualiza dados de usuario do telegram

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,42 +1,32 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import { getTelegramUserInfo, getTelegramGeneralDataInfo } from "../api/users";
+import { getTelegramUserInfo } from "~/api/users";
 import { UserCategory, UserData } from "~/api/types";
 import { getCategoryByUserId } from "~/api/firebaseConnection.server";
 import { useEffect, useState } from "react";
 
 type LoaderData = {
   userCategories: UserCategory[];
-  userData: UserData | null;
-  telegramData: any;  // Adicionado para armazenar os dados gerais do Telegram
 };
 
 export const loader = async () => {
-  let userData: UserData | null = null;
   let userCategories: UserCategory[] = [UserCategory.ANY_USER];
-  let telegramData: any = {};  // Novo para armazenar os dados gerais do Telegram
 
   try {
-    userData = getTelegramUserInfo();
-    
-    if (process.env.NODE_ENV === "production" && userData?.id) {
-      const DBUserCategory = await getCategoryByUserId(userData?.id);
-      userCategories = [DBUserCategory];
-    } else {
-      if (process.env.NODE_ENV === "development") {
-        userCategories = [
-          UserCategory.ANY_USER,
-          UserCategory.AMECICLISTAS,
-          UserCategory.PROJECT_COORDINATORS,
-          UserCategory.AMECICLO_COORDINATORS,
-        ];
-      }
+    if (process.env.NODE_ENV === "development") {
+      // Permitir o acesso a todas as categorias no ambiente de desenvolvimento, para testar cada uma comente ou apague a linha
+      userCategories = [
+        UserCategory.ANY_USER,
+        UserCategory.AMECICLISTAS,
+        UserCategory.PROJECT_COORDINATORS,
+        UserCategory.AMECICLO_COORDINATORS,
+      ];
     }
   } catch (error) {
     console.error("Error loading data:", error);
   }
 
-  return json<LoaderData>({ userCategories, userData, telegramData });
+  return json<LoaderData>({ userCategories });
 };
 
 const hasAccessToCategory = (userCategories: UserCategory[], category: UserCategory) => {
@@ -50,27 +40,19 @@ const hasAccessToCategory = (userCategories: UserCategory[], category: UserCateg
 };
 
 export default function Index() {
-  const { userCategories, userData } = useLoaderData<LoaderData>();
-
-  const [data, _setData] = useState(userData);
-  const [telegramUserData, setTelegramUserData] = useState<any>(null);
+  const { userCategories } = useLoaderData<LoaderData>();
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [telegramData, setTelegramData] = useState([]);
+  console.log(userCategories)
 
   useEffect(() => {
-    if (userData) {
-      // Após pegar os dados do usuário, faça a busca de dados adicionais do Telegram
-      const fetchTelegramData = async () => {
-        const telegramInfo = await getTelegramGeneralDataInfo();  // Aqui você pode obter os dados gerais do Telegram, se necessário
-        setTelegramUserData(telegramInfo);
-      };
-
-      fetchTelegramData();
-    }
-  }, [userData]);  // Reexecuta o useEffect sempre que o userData mudar
+    setUserData(() => getTelegramUserInfo());
+  }, []);
 
   return (
     <div className="container mx-auto py-8 px-4">
       <h1 className="text-3xl font-bold text-teal-600 text-center">
-        Ameciclobot Miniapp {telegramUserData?.id}
+        Ameciclobot Miniapp {userData?.id}
       </h1>
       <div className="mt-6">
         <Link to="/criar-evento">
@@ -81,7 +63,54 @@ export default function Index() {
             📅 Criar Evento
           </button>
         </Link>
-        {/* Outros links e botões */}
+        <Link to="/solicitar-pagamento">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.PROJECT_COORDINATORS) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.PROJECT_COORDINATORS)}
+          >
+            💰 Solicitar Pagamento
+          </button>
+        </Link>
+        <Link to="/adicionar-fornecedor">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.PROJECT_COORDINATORS) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.PROJECT_COORDINATORS)}
+          >
+            📦 Adicionar Fornecedor
+          </button>
+        </Link>
+        <Link to="/links-uteis">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.ANY_USER) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.ANY_USER)}
+          >
+            🔗 Lista de Links Úteis
+          </button>
+        </Link>
+        <Link to="/grupos-de-trabalho">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.AMECICLISTAS) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.AMECICLISTAS)}
+          >
+            👥 Grupos de Trabalho
+          </button>
+        </Link>
+        <Link to="/lista-projetos">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.AMECICLISTAS) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.AMECICLISTAS)}
+          >
+            📊 Projetos em Andamento
+          </button>
+        </Link>
+        <Link to="/user">
+          <button
+            className={`button-full ${!hasAccessToCategory(userCategories, UserCategory.ANY_USER) ? "button-disabled" : ""}`}
+            disabled={!hasAccessToCategory(userCategories, UserCategory.ANY_USER)}
+          >
+            ⚙️ Suas configurações
+          </button>
+        </Link>
       </div>
     </div>
   );
