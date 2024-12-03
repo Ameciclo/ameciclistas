71966561333bbf6fc80203adced3e3197168ec0d
Italo Sergio <italosergio@mail.com>
2024-12-03 16:45:45 -0300

    Melhora página de sucesso na solicitação do pagamento

--- app/routes/solicitar-pagamento-sucesso.tsx
+++ app/routes/solicitar-pagamento-sucesso.tsx
@@ -1,42 +1,28 @@
 // routes/sucesso.tsx
 import { useNavigate } from "@remix-run/react";
-import Unauthorized from "~/components/Unauthorized";
-import { getUserCategories } from "~/api/users";
-import { UserCategory } from "~/api/types";
 
 export default function Sucesso() {
   const navigate = useNavigate();
-  const userId = 157783985; // Substitua com a lógica real para obter o ID do usuário
-
-  // Verifica se o usuário tem permissão para acessar a página
-  const isAuthorized = getUserCategories(userId).includes(UserCategory.AMECICLISTAS);
-
-  if (!isAuthorized) {
-    return (
-      <Unauthorized
-        pageName="Página de Sucesso"
-        requiredPermission="AMECICLISTAS"
-      />
-    );
-  }
 
   return (
     <div className="container">
       <h2 className="text-primary">✅ Solicitação Enviada com Sucesso!</h2>
       <p>A sua solicitação de pagamento foi processada e está em andamento.</p>
-      
+
       <div className="button-group">
+        <br />
+        <br />
         <button
-          className="button-primary"
+          className="button-full"
           onClick={() => navigate("/solicitar-pagamento")}
         >
           Solicitar Novo Pagamento
         </button>
         <button
-          className="button-secondary"
+          className="button-secondary-full"
           onClick={() => navigate(-1)} // Voltar à página anterior
         >
-          Voltar
+          ⬅️ Voltar
         </button>
       </div>
     </div>
