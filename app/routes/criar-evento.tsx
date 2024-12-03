939aad5ad02b99f5f12618e3dbc3a09c46d9a948
Italo Sergio <italosergio@mail.com>
2024-12-03 02:04:56 -0300

    Clean code na pagina criar-evento

--- app/routes/criar-evento.tsx
+++ app/routes/criar-evento.tsx
@@ -1,11 +1,11 @@
-import { useLoaderData, useNavigate } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { useState, useEffect } from "react";
-import { getTelegramUserInfo, getUserCategories } from "../api/users";
+import { getTelegramUserInfo } from "../api/users";
 import { UserCategory, UserData } from "~/api/types";
 import { Agenda } from "../api/types"; // Importe a interface Agenda
-import { loader } from "./_index";
 import { isAuth } from "~/hooks/isAuthorized";
 import Unauthorized from "~/components/Unauthorized";
+import { loader } from "../loaders/solicitar-pagamento-loader";
 export { loader }
 
 export default function CriarEvento() {
@@ -13,7 +13,6 @@ export default function CriarEvento() {
   const [userPermissions, setUserPermissions] = useState(currentUserCategories)
   const [userInfo, setUserInfo] = useState<UserData | null>({} as UserData)
 
-  const navigate = useNavigate();
   const [titulo, setTitulo] = useState("");
   const [data, setData] = useState<string>("");
   const [hora, setHora] = useState<string>("");
@@ -21,7 +20,6 @@ export default function CriarEvento() {
   const [descricao, setDescricao] = useState("");
   const [agenda, setAgenda] = useState("");
   const [agendas, setAgendas] = useState<Agenda[]>([]);
-  const [isAuthorized, setIsAuthorized] = useState(false);
 
   useEffect(() => setUserInfo(() => getTelegramUserInfo()), []);
 
@@ -32,32 +30,6 @@ export default function CriarEvento() {
   }, [userInfo])
 
   useEffect(() => {
-    let userId;
-    const isDevelopment = process.env.NODE_ENV === "development";
-
-    if (isDevelopment) {
-      // Carregar o ID do usuário a partir do arquivo JSON em desenvolvimento
-      fetch("/devUserId.json")
-        .then((response) => response.json())
-        .then((data) => {
-          userId = data.userId;
-          checkPermissions(userId);
-        })
-        .catch((error) => {
-          console.error("Error loading the user ID from devUserId.json:", error);
-        });
-    } else {
-      const telegram = (window as any)?.Telegram?.WebApp;
-      userId = telegram?.initDataUnsafe?.user?.id;
-
-      if (!userId) {
-        console.error("User ID is undefined. Closing the app.");
-        telegram?.close();
-        return;
-      }
-      checkPermissions(userId);
-    }
-
     // Carregar as agendas do calendars.json
     fetch("/app/mockup/calendars.json")
       .then((response) => response.json())
@@ -66,11 +38,6 @@ export default function CriarEvento() {
         console.error("Error loading calendars:", error);
       });
   }, []);
-
-  const checkPermissions = (userId: number) => {
-    const userCategories = getUserCategories(userId);
-    setIsAuthorized(userCategories.includes(UserCategory.AMECICLISTAS));
-  };
 
   const handleSubmit = () => {
     try {
@@ -92,89 +59,72 @@ export default function CriarEvento() {
   return isAuth(userPermissions, UserCategory.AMECICLISTAS) ? (
     <div className="container">
       <h2 className="text-primary">📅 Criar Evento</h2>
-      {isAuthorized ? (
-        <>
-          <div className="form-group">
-            <label className="form-label">Título do Evento:</label>
-            <input
-              className="form-input"
-              type="text"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Data:</label>
-            <input
-              className="form-input"
-              type="date"
-              value={data}
-              onChange={(e) => setData(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Hora:</label>
-            <input
-              className="form-input"
-              type="time"
-              value={hora}
-              onChange={(e) => setHora(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Duração (em horas):</label>
-            <input
-              className="form-input"
-              type="number"
-              value={duracao}
-              onChange={(e) => setDuracao(e.target.value)}
-              min="0"
-              step="0.5"
-              placeholder="Ex: 1.5"
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Descrição:</label>
-            <textarea
-              className="form-input"
-              value={descricao}
-              onChange={(e) => setDescricao(e.target.value)}
-              rows={4}
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <label className="form-label">Agenda:</label>
-            <select
-              className="form-input"
-              value={agenda}
-              onChange={(e) => setAgenda(e.target.value)}
-            >
-              <option value="">Selecione uma agenda</option>
-              {agendas.map((ag, index) => (
-                <option key={index} value={ag.name}>
-                  {ag.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button className="button-full" onClick={handleSubmit}>
-            Criar Evento
-          </button>
-        </>
-      ) : (
-        <>
-          <p>Você não tem permissão para criar eventos.</p>
-          <button
-            className="button-full"
-            onClick={() => (window.location.href = "https://www.ameciclo.org")}
-          >
-            Visite o Site da Ameciclo
-          </button>
-        </>
-      )}
-      <button className="button-secondary-full" onClick={() => navigate(-1)}>
-        ⬅️ Voltar
+      <div className="form-group">
+        <label className="form-label">Título do Evento:</label>
+        <input
+          className="form-input"
+          type="text"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label">Data:</label>
+        <input
+          className="form-input"
+          type="date"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label">Hora:</label>
+        <input
+          className="form-input"
+          type="time"
+          value={hora}
+          onChange={(e) => setHora(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label">Duração (em horas):</label>
+        <input
+          className="form-input"
+          type="number"
+          value={duracao}
+          onChange={(e) => setDuracao(e.target.value)}
+          min="0"
+          step="0.5"
+          placeholder="Ex: 1.5"
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label">Descrição:</label>
+        <textarea
+          className="form-input"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          rows={4}
+        ></textarea>
+      </div>
+      <div className="form-group">
+        <label className="form-label">Agenda:</label>
+        <select
+          className="form-input"
+          value={agenda}
+          onChange={(e) => setAgenda(e.target.value)}
+        >
+          <option value="">Selecione uma agenda</option>
+          {agendas.map((ag, index) => (
+            <option key={index} value={ag.name}>
+              {ag.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button className="button-full" onClick={handleSubmit}>
+        Criar Evento
       </button>
     </div>
-  ) : <Unauthorized pageName="Solicitar Pagamentos" requiredPermission="Coordenador de Projeto"/>
+  ) : <Unauthorized pageName="Calendario" requiredPermission="Ameciclista" />
 }
