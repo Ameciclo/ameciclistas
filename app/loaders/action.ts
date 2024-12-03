4601c54a4fae2f10506d62ecd29b419bd794b7f5
Italo Sergio <italosergio@mail.com>
2024-12-03 16:42:55 -0300

    Cria Action que adiciona os Fornecedores no Banco de Dados

--- app/loaders/action.ts
+++ app/loaders/action.ts
@@ -1,20 +1,18 @@
-// actions/solicitarPagamentoAction.ts
+// actions/universalAction.ts
 import { redirect, json, ActionFunction } from "@remix-run/node";
-import { savePaymentRequest } from "~/api/firebaseConnection.server";
+import { savePaymentRequest, saveRecipient } from "~/api/firebaseConnection.server";
 
-export const action: ActionFunction = async ({ request }) => {
 
-  const formData = await request.formData();
-
+// Função para criar o pagamento
+const createPaymentRequest = (formData: FormData) => {
   const date = new Date();
-
   const formatter = new Intl.DateTimeFormat("pt-BR", {
     timeZone: "America/Sao_Paulo",
     dateStyle: "short",
     timeStyle: "medium",
   });
 
-  const paymentRequest = {
+  return {
     date: formatter.format(date),
     project: JSON.parse(formData.get("project") as string),
     budgetItem: formData.get("rubrica"),
@@ -25,15 +23,48 @@ export const action: ActionFunction = async ({ request }) => {
     group_message_id: "",
     invoice_url: "",
     from: JSON.parse(formData.get("telegramUserInfo") as string),
-    recipient_information: JSON.parse(formData.get("fornecedores") as string).find((s: any) => s.nome === formData.get("fornecedor")),
+    recipient_information: JSON.parse(formData.get("fornecedores") as string).find(
+      (s: any) => s.nome === formData.get("fornecedor")
+    ),
   };
+};
+
+// Função para criar o fornecedor
+const createFornecedorData = (formData: FormData) => ({
+  id: formData.get("cpfCnpj"),
+  name: formData.get("nomeFantasia"),
+  razaoSocial: formData.get("razaoSocial"),
+  cpfCnpj: formData.get("cpfCnpj"),
+  email: formData.get("email"),
+  telefone: formData.get("telefone"),
+  tipoChavePix: formData.get("tipoChavePix"),
+  chavePix: formData.get("chavePix"),
+});
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const actionType = formData.get("actionType"); // Identifica o tipo de ação
 
   try {
-    await savePaymentRequest(paymentRequest);
-    console.log(paymentRequest);
-    return redirect("/solicitar-pagamento-sucesso");
+    switch (actionType) {
+      case "solicitarPagamento":
+        const paymentRequest = createPaymentRequest(formData);
+        await savePaymentRequest(paymentRequest);
+        console.log(paymentRequest)
+        return redirect("/solicitar-pagamento-sucesso");
+
+      case "adicionarFornecedor":
+        const fornecedorData = createFornecedorData(formData);
+        await saveRecipient(fornecedorData);
+        console.log(fornecedorData)
+        return redirect("/adicionar-fornecedor-sucesso");
+
+      default:
+        throw new Error(`Ação não reconhecida: ${actionType}`);
+    }
   } catch (error: any) {
-    console.error("Erro ao enviar solicitação:", error);
+    console.error(`Erro ao processar a ação ${actionType}:`, error);
     return json({ error: error.message }, { status: 500 });
   }
 };
+
