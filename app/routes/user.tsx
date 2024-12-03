f3ed2293500d21e77f9c0e4cf86f4d1534ddf044
Italo Sergio <italosergio@mail.com>
2024-12-03 16:47:12 -0300

    Melhora página de Informações do Usuário

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -14,10 +14,10 @@ export default function User() {
   return (
     <div className="container mx-auto py-8 px-4">
       <h1 className="text-3xl font-bold text-teal-600 text-center">Dados do Usuário</h1>
-      
+
       {userData ? (
         <div className="mt-6 text-center">
-          <h2 className="text-xl font-semibold">Bem-vindo, {userData.first_name}!</h2>
+          <h2 className="text-xl font-semibold">Olá, {userData.first_name}!</h2>
           <ul className="mt-2">
             <li>ID: {userData.id}</li>
             <li>Nome: {userData.first_name} {userData.last_name || ''}</li>
@@ -27,10 +27,16 @@ export default function User() {
           </ul>
         </div>
       ) : (
-        <div className="mt-6 text-center">Carregando...</div>
+        <div className="mt-6 text-center">
+          {process.env.NODE_ENV === "development"
+            ? "O ambiente de Desenvolvimento não é capaz de carregar as informações de usuário telegram"
+            : "Carregando..."}
+          <br />
+          <br />
+        </div>
       )}
 
-      <Link to="/" className="mt-4 inline-block">
+      <Link to="/" className="mt-4">
         <button className="button-secondary-full">
           Voltar
         </button>
