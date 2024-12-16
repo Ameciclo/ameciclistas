f8e3e75c14c44d14bb9d1c01943cdd453edac9c5
Daniel Valença <dvalenca@gmail.com>
2024-12-16 13:27:53 -0300

    modificações menores

--- app/hooks/isAuthorized.ts
+++ app/hooks/isAuthorized.ts
@@ -1,13 +1,36 @@
 // src/utils/isAuthorized.ts
 import { UserCategory } from "~/utils/types";
 
-export const isAuth = (currentUserCategories: string[], category: UserCategory) => {
+export const isAuth = (
+  currentUserCategories: string[],
+  category: UserCategory
+) => {
   const accessHierarchy = {
-    [UserCategory.ANY_USER]: [UserCategory.ANY_USER, UserCategory.AMECICLISTAS, UserCategory.PROJECT_COORDINATORS, UserCategory.AMECICLO_COORDINATORS, UserCategory.DEVELOPMENT],
-    [UserCategory.AMECICLISTAS]: [UserCategory.AMECICLISTAS, UserCategory.PROJECT_COORDINATORS, UserCategory.AMECICLO_COORDINATORS, UserCategory.DEVELOPMENT],
-    [UserCategory.PROJECT_COORDINATORS]: [UserCategory.PROJECT_COORDINATORS, UserCategory.AMECICLO_COORDINATORS, UserCategory.DEVELOPMENT],
-    [UserCategory.AMECICLO_COORDINATORS]: [UserCategory.AMECICLO_COORDINATORS, UserCategory.DEVELOPMENT],
+    [UserCategory.ANY_USER]: [
+      UserCategory.ANY_USER,
+      UserCategory.AMECICLISTAS,
+      UserCategory.PROJECT_COORDINATORS,
+      UserCategory.AMECICLO_COORDINATORS,
+      UserCategory.DEVELOPMENT,
+    ],
+    [UserCategory.AMECICLISTAS]: [
+      UserCategory.AMECICLISTAS,
+      UserCategory.PROJECT_COORDINATORS,
+      UserCategory.AMECICLO_COORDINATORS,
+      UserCategory.DEVELOPMENT,
+    ],
+    [UserCategory.PROJECT_COORDINATORS]: [
+      UserCategory.PROJECT_COORDINATORS,
+      UserCategory.AMECICLO_COORDINATORS,
+      UserCategory.DEVELOPMENT,
+    ],
+    [UserCategory.AMECICLO_COORDINATORS]: [
+      UserCategory.AMECICLO_COORDINATORS,
+      UserCategory.DEVELOPMENT,
+    ],
     [UserCategory.DEVELOPMENT]: [UserCategory.DEVELOPMENT],
   };
-  return accessHierarchy[category]?.some((allowedCategory) => currentUserCategories.includes(allowedCategory));
+  return accessHierarchy[category]?.some((allowedCategory) =>
+    currentUserCategories.includes(allowedCategory)
+  );
 };
