379ad16bedaae9cb1b669347e1f90a88568737e5
Daniel Valença <dvalenca@gmail.com>
2025-02-12 11:09:58 -0300

    nova busca no fornecedor por nicknames

--- app/components/Forms/FornecedorInput.tsx
+++ app/components/Forms/FornecedorInput.tsx
@@ -1,6 +1,8 @@
 // components/SolicitarPagamento/FornecedorInput.tsx
-import React from "react";
-import SupplierAutocomplete from "~/components/Forms/SupplierAutocomplete";
+// components/Forms/SupplierAutocomplete.tsx
+import React, { useState } from "react";
+import Autosuggest from "react-autosuggest";
+import { Supplier } from "~/utils/types";
 import SendToAction from "../SendToAction";
 
 interface FornecedorInputProps {
@@ -8,6 +10,60 @@ interface FornecedorInputProps {
   fornecedor: string;
   setFornecedor: React.Dispatch<React.SetStateAction<string>>;
 }
+
+interface Props {
+  fornecedores: Supplier[];
+  value: string;
+  onChange: (value: string) => void;
+}
+const SupplierAutocomplete: React.FC<Props> = ({ fornecedores, value, onChange }) => {
+  const [suggestions, setSuggestions] = useState<Supplier[]>([]);
+
+  const getSuggestions = (inputValue: string) => {
+    const lowercasedInput = inputValue.toLowerCase();
+    return fornecedores
+      .filter(fornecedor =>
+        fornecedor.name.toLowerCase().includes(lowercasedInput) ||
+        (fornecedor.nickname &&
+          fornecedor.nickname.toLowerCase().includes(lowercasedInput))
+      )
+      .slice(0, 5);
+  };
+
+  const onSuggestionsFetchRequested = ({ value }: { value: string }) => {
+    setSuggestions(getSuggestions(value));
+  };
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const getSuggestionValue = (suggestion: Supplier) => suggestion.name;
+
+  const renderSuggestion = (suggestion: Supplier) => (
+    <div style={{ cursor: "pointer" }}>
+      <span>➕ </span>
+      {suggestion.nickname}
+      {suggestion.name && ` (${suggestion.name})`}
+    </div>
+  );
+
+  return (
+    <Autosuggest
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+      onSuggestionsClearRequested={onSuggestionsClearRequested}
+      getSuggestionValue={getSuggestionValue}
+      renderSuggestion={renderSuggestion}
+      inputProps={{
+        value: value,
+        onChange: (_event, { newValue }) => onChange(newValue),
+        placeholder: "Digite o nome do fornecedor",
+        className: "form-input",
+      }}
+    />
+  );
+};
 
 const FornecedorInput: React.FC<FornecedorInputProps> = ({
   fornecedores,
